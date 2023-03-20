07b2d6e2c95aad06d19f2f23b49f73178cc73ff8
shinde-darshan06 <121223564+shinde-darshan06@users.noreply.github.com>
2023-03-20 12:00:08 +0530

    Fibonacci series using recursion

--- python-practice-codes/Fibonacci series.py
+++ python-practice-codes/Fibonacci series.py
@@ -13,3 +13,20 @@ for i in range(0, num):
     lst.append(result)
     
 print("fibonacci series are: ", lst)
+
+
+# Fibonacci series using recursion
+
+num = int(input("enter number for fibonacci series: "))
+
+def fibonacci(num):
+    if num==0:
+        return 0
+    elif num==1:
+        return 1
+    else:
+        return fibonacci(num-1)+fibonacci(num-2)
+
+print("fibonacci series are:")
+for i in range(0,num):
+    print(fibonacci(i))
